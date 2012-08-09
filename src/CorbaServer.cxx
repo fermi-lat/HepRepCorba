45a849791e8da75d047adff80b3642e66fc38633
usher <>
2012-08-09 20:36:04 +0000

    Turn down the message level for omin orb

--- src/CorbaServer.cxx
+++ src/CorbaServer.cxx
@@ -16,7 +16,7 @@ void CorbaServer::run()
 {  
   int orb_argc = 1;
   char* orb_argv[] = {""};
-  const char* options[][2] = { { "traceLevel", "25" }, {"giopMaxMsgSize", "33554432" }, { 0, 0 } };
+  const char* options[][2] = { { "traceLevel", "10" }, {"giopMaxMsgSize", "33554432" }, { 0, 0 } };
   
   try {
   m_orb = CORBA::ORB_init(orb_argc, orb_argv, "omniORB4", options);
